148895d7820ffcfa783e47d3e250ecf0d578dd79
Atonu Chakrabortty <83840853+atonu1971@users.noreply.github.com>
2022-03-16 11:20:28 -0500

    docstring and function code

--- data_analysis_iris.py
+++ data_analysis_iris.py
@@ -4,5 +4,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
+iris_df = pd.read_csv("iris.csv")
+
+setosa = iris_df[iris_df.species == "Iris_setosa"]
+versicolor = iris_df[iris_df.species == "Iris_versicolor"]
+virginica = iris_df[iris_df.species == "Iris_virginica"]
+
+species = [setosa, versicolor, virginica]
+
+def get_regression_plot(species)
+    """
+    Parameters
+    -------------------------------
+    species: pandas.core.frame.DataFrame
+        A dataframe containing morphological measurements for a given species.
+    Returns
+    -------------------------------
+    A scatterplot .png file showing the data with the regression of the x & y 
+    variables plotted onto them.
+    """
+    x = species.petal_length_cm
+    y = species.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept   
+    plt.scatter(x, y, label = 'Data')
+    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("petal_v_sepal_length_regress.png")
+    return 
+
+    #for taxon in species:
+    #    get_regression_plot(taxon)
 
 if __name__ == '__main__':
+    main()
